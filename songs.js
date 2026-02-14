be0c9fa90b26eda04eb8cbc076cfd7e8a50e1cc7
ghosheep5274-creator <ghosheep5274@gmail.com>
2026-02-15 00:40:38 +0900

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,15 @@
+// songs.js - 純索引版
+const songLibrary = {
+    "mic_drop": {
+        title: "🎤 MIC Drop",
+        videoId: "e95-Gaj2iXM",
+        bpm: 170,
+        file: "lyrics/mic_drop.json" // 指向檔案
+    },
+    "dna": {
+        title: "🧬 DNA",
+        videoId: "MBdVXkSdhwU",
+        bpm: 130,
+        file: "lyrics/dna.json"
+    }
+};
