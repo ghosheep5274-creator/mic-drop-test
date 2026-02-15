c6ddcc166e5a14cf4427c144002d227c00d41fe5
ghosheep5274-creator <ghosheep5274@gmail.com>
2026-02-15 21:43:45 +0900

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,13 +3,13 @@ const songLibrary = {
     "mic_drop": {
         title: "🎤 MIC Drop",
         videoId: "Zos_33vzX0w",
-        bpm: 85,
+        bpm: 170,
         file: "lyrics/mic_drop.json" // 指向檔案
     },
     "dna": {
         title: "🧬 DNA",
         videoId: "G3o94Cajmrs",
-        bpm: 65,
+        bpm: 130,
         file: "lyrics/dna.json"
     }
 };
