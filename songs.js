5120189354d849d6d03eede2dc9becd054a847aa
ghosheep5274-creator <ghosheep5274@gmail.com>
2026-02-18 01:26:25 +0900

    Update songs.js

--- songs.js
+++ songs.js
@@ -55,7 +55,7 @@ const songLibrary = {
         file: "lyrics/dynamite.json"
      },
     "butter": {
-        title: "🧈 Butter-test",
+        title: "🧈 Butter",
         videoId: "OeEWAgUhlbI",
         bpm: 55,
         file: "lyrics/butter.json"
